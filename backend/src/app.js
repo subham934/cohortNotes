77943c57f85aff6fbb2e303f0478048e437773d7
Subham Dhar <43316699+subham934@users.noreply.github.com>
2026-07-23 21:16:56 +0530

    Add files via upload

---
+++ backend/src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json())
+
+app.get("/", (req, res)=>{
+    res.send("Hello, World!!")
+})
+
+
+app.get("/api/data", (req, res)=>{
+    const data = {
+        message: "This is some sample data from the API.",
+        timestamp: new Date()
+    }
+    res.json(data)
+})
+
+app.get("/api/users", (req, res)=>{
+  const users = [
+    {id: 1, name: "John Doe", email: "[EMAIL_ADDRESS]", role: "admin"},
+    {id: 2, name: "Jane Doe", email: "[EMAIL_ADDRESS]", role: "user"},
+    {id: 3, name: "Bob Smith", email: "[EMAIL_ADDRESS]", role: "user"},
+    {id: 4, name: "Ron Weasley", email: "[EMAIL_ADDRESS]", role: "user"},
+  ]
+
+  return res.status(200).json(users)   
+})
+
+export default app;
